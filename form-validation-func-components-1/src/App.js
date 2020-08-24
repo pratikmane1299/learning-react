32808d3e45a1012772d4215141ee3c5524c4d53e
pratik mane <pratik.mane1299@gmail.com>
2020-08-24 20:08:25 +0530

    create basic form with react-hooks-form

--- form-validation-func-components-1/src/App.js
+++ form-validation-func-components-1/src/App.js
@@ -1,10 +1,68 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
 import './App.css';
 
 function App() {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h2>React form validation using React-Form-Hooks</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label>Name</label>
+          <input 
+            type="text" 
+            name="name" 
+            ref={register} 
+          />
+        </div>
+
+        <div>
+          <label>Email</label>
+          <input 
+            type="text" 
+            name="email" 
+            ref={register} 
+          />
+        </div>
+
+        <div>
+          <label>Age</label>
+          <input 
+            type="text" 
+            name="age" 
+            ref={register} 
+          />
+        </div>
+
+        <div>
+          <label>Gender</label>
+          <select 
+            name="gender" 
+            ref={register}
+          >
+            <option>Select</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+        </div>
+
+        <div>
+          <label>Password</label>
+          <input 
+            type="password" 
+            name="password" 
+            ref={register} 
+          />
+        </div>
+
+        <div>
+          <button>Submit</button>
+        </div>
+      </form>
     </div>
   );
 }
